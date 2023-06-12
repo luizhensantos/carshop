ef9948c4b8b483e0dedaba58a36b9728a32135fe
Luiz Henrique <luizhenrique@iMac.local>
2023-06-12 13:58:17 -0300

    iniciando projeto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Alert, Button} from "react-bootstrap";
+
+function App() {
+  return (
+    <div className="App">
+    <Button variant="primary"> enter</Button>
+    </div>
+  );
+}
+
+export default App;
